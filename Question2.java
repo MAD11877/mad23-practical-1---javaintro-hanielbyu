1b883f04b75b6a66a2337926477929874f3d0200
hanielbyu <116860359+hanielbyu@users.noreply.github.com>
2023-04-20 11:02:55 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -5,11 +5,11 @@ public class Question2
   public static void main(String[] args)
   {
     Scanner myObj = new Scanner(System.in);
-    System.out.println("Enter Height(m): ");
+//     System.out.println("Enter Height(m): ");
     double height = myObj.nextDouble();  
-    System.out.println("Enter Weight(kg): ");
+//     System.out.println("Enter Weight(kg): ");
     double weight = myObj.nextDouble();
     double bmi = weight/(height*height);
-    System.out.println("BMI is: " + bmi);
+    System.out.println(bmi);
   }
 }
